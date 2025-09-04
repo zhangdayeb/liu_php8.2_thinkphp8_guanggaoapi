1c27927fc70b0f9f55844da18b4b37425e3bb7d7
zhangdayeb <you@example.com>
2025-09-04 15:39:17 +0800

    'up'

---
+++ app/controller/ad/AD.php
@@ -0,0 +1,14 @@
+<?php
+namespace app\controller\ad;
+
+use app\BaseController;
+use think\facade\Db;
+use think\facade\Log;
+
+class UpdateApiGroupGames extends BaseController
+{
+    public function get_ad_list()
+    {
+
+    }
+}
---
+++ route/app.php
@@ -0,0 +1,19 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006~2018 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liu21st <liu21st@gmail.com>
+// +----------------------------------------------------------------------
+use think\facade\Route;
+
+Route::get('think', function () {
+    return 'hello,ThinkPHP8!';
+});
+Route::get('hello/:name', 'index/hello');
+
+// 计划模块
+Route::rule('ad/ad_list$', 'ad.Ad/get_ad_list'); // 获取广告列表 
